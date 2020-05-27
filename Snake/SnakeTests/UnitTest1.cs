3764d7705f78795289dc1169739150f51bb83c16
KiraEun <44110267+KiraDragon@users.noreply.github.com>
2020-05-27 20:37:08 +0800

    Attempt 3

---
+++ Snake/SnakeTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Snake;
+
+namespace SnakeTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}

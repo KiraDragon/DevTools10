aab43eb319803f97696f2b39890a5b9498018fd8
KiraEun <44110267+KiraDragon@users.noreply.github.com>
2020-05-27 23:00:35 +0800

    Finally got it to work + did my test

---
+++ SnakeTest/SnakeTest/Tester.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+using System.Threading;
+using System.IO;
+
+
+namespace SnakeTest
+{
+    public struct Position
+    {
+        public int row;
+        public int col;
+        public Position(int row, int col)
+        {
+            this.row = row;
+            this.col = col;
+        }
+    }
+
+    public class Tester
+    {
+        public string SaveScore(string username, int score)
+        {
+            StreamWriter sw = new StreamWriter("../../scoreboard.txt", true);
+            string entry;
+            entry = "{0}: {1} points";
+            entry = string.Format(entry, username, score.ToString());
+            sw.WriteLine(entry);
+            sw.Close();
+            //modified to return entry
+            return entry; 
+        }
+
+        public List<Position> MakeObstacles(Random rng)
+        {
+            List<Position> obstacles = new List<Position>();
+            for (int i = 0; i < 5; i++)
+            {
+                obstacles.Add(new Position(rng.Next(6, Console.WindowHeight - 6),
+                            rng.Next(6, Console.WindowWidth - 6)));
+            };
+            foreach (Position obstacle in obstacles)
+            {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.SetCursorPosition(obstacle.col, obstacle.row);
+                Console.Write("▒");
+            }
+
+            return obstacles;
+        }
+
+        public List<Position> MakeNewObstacles(Random rng, List<Position> obstacles, Position food, Queue<Position> snakeElements)
+        {
+
+            //modified to ensure no infinite loops
+            Position obstacle = new Position();
+          
+            obstacle = new Position(rng.Next(6, Console.WindowHeight - 6),
+            rng.Next(6, Console.WindowWidth - 6));
+            
+
+
+            obstacles.Add(obstacle);
+            
+            Console.SetCursorPosition(obstacle.col, obstacle.row);
+            
+            Console.ForegroundColor = ConsoleColor.Cyan;
+  
+            Console.Write("▒");
+
+            //modified to return obstacles
+            return obstacles; 
+        }
+    }
+}
---
+++ SnakeTest/SnakeTester/UnitTest1.cs
@@ -0,0 +1,32 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+using System.Threading;
+using System.IO;
+using SnakeTest; 
+
+namespace SnakeTester
+{ 
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Test1()
+        {
+            Random rng = new Random(); 
+            Tester test = new Tester();
+            Position food = new Position();
+            Queue<Position> snakeElements = new Queue<Position>();
+            for (int i = 0; i <= 3; i++)
+            {
+                snakeElements.Enqueue(new Position(0, i));
+            }
+            List<Position> testObs = test.MakeObstacles(rng);
+            List<Position> testObs2 = test.MakeNewObstacles(rng, testObs, food, snakeElements);
+            Assert.AreEqual(6, testObs2.Count);
+        }
+    }
+}
